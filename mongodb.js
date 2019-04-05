e32d67de575ada667ae0553d7c236fef7c3e6e5e
Aldo Cedillo <mr_jenkinshlcn@hotmail.com>
2019-04-05 17:42:13 -0600

    add more practices with read documents

--- mongodb.js
+++ mongodb.js
@@ -13,14 +13,43 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     }
     const db = client.db(databaseName);
 
-    db.collection('users').findOne({
-        name: 'Jen',
-        age: 1
-    }, (error, user) => {
+    // db.collection('users').findOne({
+    //     _id: new ObjectID('5ca7d0c86ec47220476f64a3')
+    // }, (error, user) => {
+    //     if (error) {
+    //         return console.log('Unable to fetch');
+    //     }
+    //     console.log(user);
+    // });
+
+    // db.collection('users').find({
+    //     age: 27
+    // }).toArray((error, users) => {
+    //    console.log(users) 
+    // })
+
+    // db.collection('users').find({
+    //     age: 27
+    // }).count((error, count) => {
+    //    console.log(count) 
+    // })
+
+    db.collection('tasks').findOne({
+        _id: new ObjectID('5ca7cc9c51bd7a1eb5f76a2e')
+    }, (error, task) => {
         if (error) {
-            return console.log('Unable to fetch');
+            return console.log(error);
         }
-        console.log(user);
+        console.log(task);
+    });
+
+    db.collection('tasks').find({
+        completed: false
+    }).toArray((error, tasks) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log(tasks);
     })
 });
 
