bdf4f6413e04eb6845932638790d6484025f670b
Daniel Carrizales <d.carrizales@hlcn.mx>
2019-07-04 18:01:43 -0500

    applying bcryptjs library to hash the passwords of the users
    Signed-off-by: Daniel Carrizales <d.carrizales@hlcn.mx>

--- src/routers/users.js
+++ src/routers/users.js
@@ -42,13 +42,18 @@ router.patch('/users/:id', async (req, res) => {
   }
   const _id = req.params.id;
   try {
-      const user = await User.findByIdAndUpdate(_id, req.body, { new:true, runValidators: true });
-      if (!user) {
-          return res.status(404).send();
-      }
-      res.send(user);
+    //   const user = await User.findByIdAndUpdate(_id, req.body, { new:true, runValidators: true });
+    const user = await User.findById(req.params.id);
+
+    updates.forEach((update) => user[update] = req.body[update]);
+    await user.save();
+
+    if (!user) {
+        return res.status(404).send();
+    }
+    res.send(user);
   } catch (error) {
-      res.status(400).send(error);
+    res.status(400).send(error);
   }
 });
 
