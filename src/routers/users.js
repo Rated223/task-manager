d3b475b54b506687cc92d1b55c270f41766ba16f
Daniel Carrizales <d.carrizales@hlcn.mx>
2019-07-08 17:05:35 -0500

    Implementing a middleware for authentication
    Signed-off-by: Daniel Carrizales <d.carrizales@hlcn.mx>

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const User = require('../models/user')
+const User = require('../models/user');
+const auth = require('../middleware/auth');
 const router = new express.Router();
 
 router.post('/users', async (req, res) => {
@@ -23,10 +24,9 @@ router.post('/users/login', async (req, res) => {
     }
 });
 
-router.get('/users', async (req, res) => {
+router.get('/users/myprofile', auth, async (req, res) => {
   try {
-      const users = await User.find({});
-      res.send(users);
+      res.send(req.user);
   } catch (error) {
       res.status(500).send();
   }
