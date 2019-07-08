f0750845359c616320ad6aee7391a4ce3fcc7b23
Daniel Carrizales <rated223@outlook.com>
2019-07-07 22:35:20 -0500

    Implementing tokens for the login
    Signed-off-by: Daniel Carrizales <rated223@outlook.com>

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -39,7 +40,13 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Age must be a postive number')
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
 userSchema.statics.findByCredentials = async (email, pass) => {
@@ -55,6 +62,14 @@ userSchema.statics.findByCredentials = async (email, pass) => {
     }
 
     return user;
+}
+
+userSchema.methods.generateAuthToken = async function() {
+    const user = this;
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse');
+    user.tokens = user.tokens.concat({ token });
+    await user.save();
+    return token;
 }
 
 // hash the plain text password
